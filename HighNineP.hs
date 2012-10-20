515dcf5fe2d8ea02e636eb96f6dad61ad59f03f8
Sergey Alirzaev <zl29ah@gmail.com>
2012-10-20 23:22:06 +0000

    Whoops! Tied to Data.NineP

--- HighNineP.hs
+++ HighNineP.hs
@@ -6,39 +6,25 @@ import qualified Data.ByteString.Char8 as B
 import FidMap
 import Data.Map
 import Data.Word
-
-data QID = QID Word8 Word32 Word64
-data NineStat = NineStat {
-		sType :: Word16,
-		sDev :: Word32,
-		sQID :: QID,
-		sMode :: Word32,
-		sATime :: Word32,
-		sMTime :: Word32,
-		sLength :: Word64,
-		sName :: B.ByteString,
-		sUID :: B.ByteString,
-		sGID :: B.ByteString,
-		sMUID :: B.ByteString
-	}
+import Data.NineP
 
 data NineFile =
 	RegularFile {
         	nineRead :: Word32 -> Word32 -> IO (B.ByteString),
         	nineWrite :: Word64 -> B.ByteString -> IO (Word32),
 		remove :: IO (),
-		stat :: IO NineStat,
-		wstat :: NineStat -> IO ()
+		stat :: IO Stat,
+		wstat :: Stat -> IO ()
 	} | Directory {
 		getFiles :: IO (Map B.ByteString NineFile),
 		remove :: IO (),
-		stat :: IO NineStat,
-		wstat :: NineStat -> IO ()
+		stat :: IO Stat,
+		wstat :: Stat -> IO ()
 	}
 
 data NineClient = NineClient {
         fidMap :: FidMap NineFile
 }
 
-boringStat :: NineStat
-boringStat = NineStat 0 0 (QID 0 0 0) 0 0 0 0 "boring" "" "" ""
+boringStat :: Stat
+boringStat = Stat 0 0 (Qid 0 0 0) 0 0 0 0 "boring" "" "" ""
