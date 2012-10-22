8a7a2d0f6d4f8d854de35ae729f3822aecbdd20f
Sergey Alirzaev <zl29ah@gmail.com>
2012-10-22 15:18:27 +0000

    make FidMap more like Data.Map

--- FidMap.hs
+++ FidMap.hs
@@ -1,11 +1,13 @@
 module FidMap (
-     emptyFidMap
-    ,insertFid
-    ,findByFid
+     empty
+    ,insert
+    ,(!)
     ,FidMap
+    ,fromList
     ) where
 
-import Data.Tree.RBTree
+import Data.Tree.RBTree (RBTree)
+import qualified Data.Tree.RBTree as RB
 import Data.Word
 
 newtype Pair a b = Pair {fromPair :: (a, b)}
@@ -19,17 +21,18 @@ instance (Ord a) => Ord (Pair a b) where
 
 newtype FidMap a = FidMap (RBTree (Pair Word32 a))
 
-emptyFidMap :: FidMap a
-emptyFidMap = FidMap emptyRB
+empty :: FidMap a
+empty = FidMap RB.emptyRB
 
-insertFid :: FidMap a -> Word32 -> a -> FidMap a
-insertFid (FidMap m) fid v = FidMap $ insertOrd m $ Pair (fid, v)
+insert :: Word32 -> a -> FidMap a -> FidMap a
+insert fid v (FidMap m) = FidMap $ RB.insertOrd m $ Pair (fid, v)
 
 --deleteFid :: FidMap a -> 
 
 
-findByFid :: FidMap a -> Word32 -> Maybe a
-findByFid (FidMap m) fid = fmap (snd.fromPair) $ searchFast cmp m fid
+(!) :: FidMap a -> Word32 -> Maybe a
+(!) (FidMap m) fid = fmap (snd.fromPair) $ RB.searchFast cmp m fid
     where   cmp fid1 (Pair (fid2, _)) = compare fid1 fid2
 
-
+fromList :: [(Word32, a)] -> FidMap a
+fromList l = FidMap $ RB.insertOrdList RB.emptyRB $ map Pair l
